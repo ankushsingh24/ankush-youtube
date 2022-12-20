dcaf763ff017304dc1a7c0c39416ae96ccf327ee
Ankush Singh <ankushsahil45@gmail.com>
2022-12-20 19:12:59 +0530

    added constants

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import HomeIcon from "@material-ui/icons/Home";
+import CodeIcon from "@material-ui/icons/Code";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import SchoolIcon from "@material-ui/icons/School";
+import FaceRetouchingNaturalIcon from "@material-ui/icons/FaceRetouchingNatural";
+import CheckroomIcon from "@material-ui/icons/Checkroom";
+import GraphicEqIcon from "@material-ui/icons/GraphicEq";
+import TheaterComedyIcon from "@material-ui/icons/TheatreComedy";
+import FitnessCenterIcon from "@material-ui/icons/FitnessCenter";
+import DeveloperModeIcon from "@material-ui/icons/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "JS Mastery", icon: <CodeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "JavaScript Mastery";
+export const demoVideoTitle =
+  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+export const demoProfilePicture =
+  "https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s176-c-k-c0x00ffffff-no-rj";
