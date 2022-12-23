f0842a044962f3ac9bafa934fea7ab6819dd0cdf
Ankush Singh <ankushsahil45@gmail.com>
2022-12-23 22:04:10 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { ChannelDetail, Feed, SearchFeed, VideoDetail } from "./components";
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
